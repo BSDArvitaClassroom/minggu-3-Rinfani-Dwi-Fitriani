aca1ca327705eaf235777ee7e5e0532c4ce660b2
Rinfani Dwi Fitriani <e42221715@student.polije.ac.id>
2024-02-22 17:48:07 +0700

    Minggu 3

---
+++ Acara 10/lingkaran.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  double phi, jarijari;
+  phi = 3.14;
+  jarijari = 10;
+  var luasLingkaran = phi * jarijari * jarijari;
+  print(luasLingkaran);
+}
---
+++ Acara 12/contoh.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) async {
+  var t = Titan(); // initialisasi t = object dari class titan
+  print("zeke"); // mencetak zeke
+  print(t.name); // akan mencetak string name yang pertama yaitu eren yeger
+  await t
+      .getName(); // masuk kedalam async await untuk mendelayed object di dalamnya //dan akan mencetak get name [done]
+  print(
+      t.name); //mencetak grisha yeger karena sudah di masukan kedalam async nya
+  print("rener"); // akan mencetak rener
+}
+
+class Titan {
+  String name = "Eren Yeger"; // mengisi value name eren yeger
+  Future<void> getName() async {
+    // masuk kedalam Future dan object get name dan inisialisasi async
+    await Future.delayed(
+        Duration(seconds: 3)); // memberi delayed pada object selama 3 detik
+    name = "grisha"; //set name grisha
+    print("get name [done]"); //cetak prin get name done
+  }
+}
